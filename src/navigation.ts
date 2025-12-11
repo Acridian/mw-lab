c93018d2fd4a34c6a8930d74bfb4cdc58ab4708f
Dmitry Smirnov <killdeluxe@gmail.com>
2025-12-11 08:39:37 +0000

    feat: update navigation and landing page content to support Russian localization

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,28 +3,11 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
-      links: [
-        {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
+      text: 'Главная',
+      href: getPermalink('/'),
     },
     {
-      text: 'Pages',
+      text: 'Продукция',
       links: [
         {
           text: 'Features (Anchor Link)',
@@ -57,36 +40,7 @@ export const headerData = {
       ],
     },
     {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
+      text: 'Публикации',
       links: [
         {
           text: 'Blog List',
@@ -111,7 +65,7 @@ export const headerData = {
       ],
     },
     {
-      text: 'Widgets',
+      text: 'Контакты',
       href: '#',
     },
   ],
