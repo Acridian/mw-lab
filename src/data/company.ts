6e6fe46ce53ad16130b10a12bc09aae31fee59fb
Dmitry Smirnov <dingier-organdy-0a@icloud.com>
2026-08-02 20:40:32 +0100

    Fixed broken build

--- src/data/company.ts
+++ src/data/company.ts
@@ -2,9 +2,11 @@ export const SITE_URL = 'https://mw-lab.ru';
 export const COMPANY_NAME = 'Лаборатория Микроволн';
 export const COMPANY_EMAIL = 'info@mw-lab.ru';
 export const COMPANY_PHONE = '+7 (917) 501-2121';
+export const COMPANY_PHONE_DISPLAY = '+7 (917) 501-2121';
 export const COMPANY_ADDRESS = {
   postalCode: '124460',
   locality: 'Москва, Зеленоград',
   street: 'проезд 4801, д. 7, стр. 5',
   country: 'RU',
 };
+export const YANDEX_MAPS_CONSTRUCTOR = 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Acc115f1f87ad892cb54ffe3100ee510d6bd76212876d3c7895c13aa0f49bbd49&width=100%25&height=400&lang=ru_RU&scroll=true';
